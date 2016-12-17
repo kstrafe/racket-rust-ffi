0a2732f4c4ae6a3ce5321e97c6b9caa730367008
Kevin Robert Stravers <macocio@gmail.com>
2016-12-17 20:26:44 +0100

    Use local path

--- main.rkt
+++ main.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 [require ffi/unsafe ffi/unsafe/define]
-[define-ffi-definer define-rust (ffi-lib "/home/kefin/stuf/docs/racket-rust/target/debug/libffi_example")]
+[define-ffi-definer define-rust (ffi-lib [string-append [path->string [current-directory]] "target/debug/libffi_example"])]
 [define-rust something (_fun -> _int)]
 [displayln "This is from inside racket!"]
 [displayln [format "rust code returned ~a" [something]]]
